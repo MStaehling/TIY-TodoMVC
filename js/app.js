bb7a465923b49940d4ea1027acbdaa28f43f14a3
Michael Staehling <Michael.staehling@gmail.com>
2015-05-14 16:16:06 -0400

    cleared the initial fielded

--- js/app.js
+++ js/app.js
@@ -20,6 +20,10 @@
             if ( event.keyCode === 13 ){
                 var task = document.querySelector('input.new-todo').value;
                 todos.taskList.push(task);
+
+							  document.querySelector('input.new-todo').value= "";
+
+
                 console.log(todos.taskList);
                 }
             // TO ADD A NEW TASK:
