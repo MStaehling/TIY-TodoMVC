8495ac2614c3f0911f79b0d1ee80f9a8952b3041
Michael Staehling <Michael.staehling@gmail.com>
2015-05-13 20:16:38 -0400

    @jorgehjr84 @sjoyal @MStaehling

--- js/app.js
+++ js/app.js
@@ -4,3 +4,6 @@
 	// Your starting point. Enjoy the ride!
 
 })(window);
+//research this and that, the enter key as an eventlistener and submit eventlistener
+//object literals, modinitzer, talked about using an array to list, cried alot.
+//worked on user stories
